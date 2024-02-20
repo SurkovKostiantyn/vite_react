bc3c8100a803dc5a7db20c77420fd03119f6a982
SurkovKostiantyn <kskrua@gmail.com>
2024-02-20 19:26:33 +0200

    оновлений код

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Nav from './components/Nav';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import Welcome from './components/Welcome';
-import Field from './components/Field';
+import Nav from './components/Nav'; // import Nav component
+import MainContent from './components/MainContent'; // import MainContent component
+import Footer from './components/Footer'; // import Footer component
+import Welcome from './components/Welcome'; // import Welcome component
+import Chat from './components/Chat.jsx'; // import Chat component
 
 function App() {
-
   return (
     <>
         <Nav/>
         <MainContent/>
-        <Welcome name={"Діма"} lastname={"Приймак"}/>
-        <Field label={"Введіть текст"} placeholder={"..."}/>
+        <Welcome name={"Текст, що завжди видно"} lastname={"Текст, який не видно"}/>
+        <Chat label={"Введіть текст"} placeholder={"..."}/>
         <Footer/>
     </>
   )
