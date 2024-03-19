d495c06e3dd997ab7785eb7dcfa979b6b6ed6d07
SurkovKostiantyn <kskrua@gmail.com>
2024-03-19 12:50:58 +0200

    firebase auth Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,36 @@
+// src/components/Login.jsx
+
+import { useState } from 'react';
+import { auth } from '../firebase';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            // Обробка успішного входу
+        } catch (error) {
+            // Обробка помилок
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+}
+
+export default Login;
