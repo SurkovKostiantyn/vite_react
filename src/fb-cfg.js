af37bffb09d9f7434cff42f1caf112c1ebc62302
SurkovKostiantyn <kskrua@gmail.com>
2024-03-19 12:31:22 +0200

    firebase

--- src/fb-cfg.js
+++ src/fb-cfg.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDej53L4lf07uexpgL7HGpPk8F6PRNVqbM",
-    authDomain: "kn20-b22a3.firebaseapp.com",
-    projectId: "kn20-b22a3",
-    storageBucket: "kn20-b22a3.appspot.com",
-    messagingSenderId: "108815732004",
-    appId: "1:108815732004:web:13ba307cb46192e651f7b1",
-    measurementId: "G-2HR10JPW9G"
+//
 };
 
 // Initialize Firebase
