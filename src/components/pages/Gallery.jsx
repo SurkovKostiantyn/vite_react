b08feca42a585af8935851476c9c4c54afb3a5e3
Maryana232 <yanatof243259@gmail.com>
2024-03-26 14:48:43 +0200

    galery + upload

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,19 +1,79 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import {ThemeContext} from "../ThemeContext.jsx";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
+// file upload to firebase storage
+import {getDownloadURL, getStorage, listAll, ref, uploadBytes} from "firebase/storage";
+
+const fetchImageUrls = async () => {
+    const storage = getStorage();
+    const listRef = ref(storage, 'some-directory/');
+
+    try {
+        const res = await listAll(listRef);
+        const urlPromises = res.items.map((itemRef) => getDownloadURL(itemRef));
+        return await Promise.all(urlPromises);
+    } catch (error) {
+        console.error("Error fetching images: ", error);
+        return [];
+    }
+};
+
+// Припускаємо, що 'file' - це файл, отриманий від <input type="file"/>
+const uploadFile = async (file) => {
+    // Перевірка чи файл є зображенням
+    if (!file.type.startsWith('image/')) {
+        console.error('Not an image file');
+        return null;
+    }
+
+    // Перевірка розміру файлу (2MB = 2 * 1024 * 1024 bytes)
+    if (file.size > 2 * 1024 * 1024) {
+        console.error('File is too large. Max size is 2MB.');
+        return null;
+    }
+
+    const storage = getStorage();
+    const storageRef = ref(storage, 'some-directory/' + file.name);
+
+    try {
+        const snapshot = await uploadBytes(storageRef, file);
+        const downloadURL = await getDownloadURL(snapshot.ref);
+        console.log('File uploaded! Accessible at', downloadURL);
+        return downloadURL;
+    } catch (error) {
+        console.error('Upload failed', error);
+        // Обробка помилок
+    }
+};
+
 
 const Gallery = () => {
     const { lightMode } = useContext(ThemeContext);
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true
+    const [images, setImages] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const handleFileSelect = (e) => {
+        setSelectedFile(e.target.files[0]);
     };
+    const handleUpload = async () => {
+        if (selectedFile) {
+            const downloadURL = await uploadFile(selectedFile);
+            if (downloadURL) {
+                setImages((prevImages) => [...prevImages, downloadURL]);
+            }
+        }
+    };
+
+    useEffect(() => {
+        const loadImages = async () => {
+            const urls = await fetchImageUrls();
+            setImages(urls);
+        };
+
+        loadImages();
+    }, []);
+
+    useEffect(() => {
+        window.dispatchEvent(new Event('resize'));
+    }, []);
 
     return (
         <div
@@ -24,29 +84,14 @@ const Gallery = () => {
             }}
         >
             <h1>Gallery</h1>
-            <Slider
-                {...settings}
-                style={{
-                    width: "100%",
-                    maxWidth: "100%",
-                    boxSizing: "border-box",
-                    padding: "0",
-                    margin: "0",
-                }}
-            >
-                <div className={"image"}>
-                    <h3>1</h3>
-                </div>
-                <div className={"image"}>
-                    <h3>2</h3>
-                </div>
-                <div className={"image"}>
-                    <h3>3</h3>
-                </div>
-                <div className={"image"}>
-                    <h3>4</h3>
-                </div>
-            </Slider>
+            <input type="file" onChange={handleFileSelect} />
+            <button onClick={handleUpload}>Надіслати</button> {/* Додана кнопка надіслати */}
+            
+            <div>
+                {images.map((url, index) => (
+                    <img src={url} style={{ width: "100%" }}  alt={'x'} key={index} />
+                ))}
+            </div>
         </div>
     )
 }
